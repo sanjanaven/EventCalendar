1d45f78d29a657ef86ffd468f8e3db11ee20c02b
sanjanaven <65728470+sanjanaven@users.noreply.github.com>
2021-01-30 18:14:13 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+//----define moment varibles used to show time----//
+var date = moment().format("MMM Do YY");
+// var currentTime = moment().format('H');
+// console.log(currentTime);
+
+//-------append date----------//
+$("#currentDay").append(date);
+
+
+//---------background Updater------------------------//
+
+var eventNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+
+//--onclick event to save user input to local storage---//
+$(".rowBtn").on("click", function() {
+    var eventNumber = $(this).parent().attr("id");
+    var textContent = $("input").val().trim();
+
+    localStorage.setItem(eventNumber, textContent);
+    console.log(eventNumber, textContent);
+});
+
+//-------Getting individual childern and setting to parent-----//
+  $("#1").children("input").val(localStorage.getItem("1"));
