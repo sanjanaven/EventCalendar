a5e54b3e741b7e8a8e4a7e2274fe9287bf26ace4
sanjanaven <65728470+sanjanaven@users.noreply.github.com>
2021-01-31 09:58:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-//$(document).ready(function() {
+$(document).ready(function() {
   // listen for save button clicks
-//  $(".saveBtn").on("click", function() {
+  $(".saveBtn").on("click", function() {
     // get nearby values
-//    var textContent = $("input").val().trim();
- //   var name = $(this).parent().attr("id");
+    var textContent = $(this).siblings(".description").val();
+    var name = $(this).parent().attr("id");
 
-    // save in localStorage
-//    localStorage.setItem(name, textContent);
+     save in localStorage
+    localStorage.setItem(name, textContent);
   });
 
-var HTML = "index.html"; //html of the page goes here
-localStorage.setItem("content", HTML);
-document.write(localStorage['content']);
 
   
   // load any saved data from localStorage
-//   $("first").val(localStorage.getItem("first"));
-   // $("first.description").val(localStorage.getItem("first"));
+  $("first.input-group-text").val(localStorage.getItem("first"));
+  //  $("first.description").val(localStorage.getItem("first"));
   // JSON.parse(window.localStorage.getItem('first'));
 
 
