b500ea165bbc09264db68dfeecb4e93178d4ee34
sanjanaven <65728470+sanjanaven@users.noreply.github.com>
2021-01-30 22:48:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
-// get the date
-var date = new Date();
-// date as a string
-var n = date.toDateString();
-// time as a string
-var time = date.toLocaleTimeString();
+$(document).ready(function() {
+  // listen for save button clicks
+  $(".saveBtn").on("click", function() {
+    // get nearby values
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-// log the date
-console.log('date:', n);
-// log the time
-console.log('time:',time);
+    // save in localStorage
+    localStorage.setItem(time, value);
+  });
 
+  // load any saved data from localStorage
+  // $("#hour-9 .description").val(localStorage.getItem("first"));
+  JSON.parse(window.localStorage.getItem('first'));
+ // $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+ // $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+ // $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+ // $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+ // $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+ // $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+ // $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+ // $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-// variables for time
-//var date = moment().format("MMM Do YY");
-// var currentTime = moment().format('H');
-// console.log(currentTime);
-
-//-------append date----------//
-//$("#currentDay").append(date);
-
-
-//---------background Updater------------------------//
-
-var eventNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-
-
-//--onclick event to save user input to local storage---//
-$(".rowBtn").on("click", function() {
-    var eventNumber = $(this).parent().attr("id");
-    var textContent = $("input").val().trim();
-
-    localStorage.setItem(eventNumber, textContent);
-    console.log(eventNumber, textContent);
+  // display current day on page
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
-
-//-------Getting individual childern and setting to parent-----//
-  $("#1").children("input").val(localStorage.getItem("1"));
