a0ade0561acddc9f2c037e1259b9ad1701e7debb
sanjanaven <65728470+sanjanaven@users.noreply.github.com>
2021-01-31 00:33:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,17 +12,10 @@ $(document).ready(function() {
 
   
   // load any saved data from localStorage
-  // $("#hour-9 .description").val(localStorage.getItem("first"));
+  // $("first.description").val(localStorage.getItem("first"));
   JSON.parse(window.localStorage.getItem('first'));
- // $("#hour-10 .description").val(localStorage.getItem("hour-10"));
- // $("#hour-11 .description").val(localStorage.getItem("hour-11"));
- // $("#hour-12 .description").val(localStorage.getItem("hour-12"));
- // $("#hour-13 .description").val(localStorage.getItem("hour-13"));
- // $("#hour-14 .description").val(localStorage.getItem("hour-14"));
- // $("#hour-15 .description").val(localStorage.getItem("hour-15"));
- // $("#hour-16 .description").val(localStorage.getItem("hour-16"));
- // $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
 
   // display current day on page
-  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+  $("#currentDay").text(moment().format("MM DD YYYY"));
 });
