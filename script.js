929859593595866bbe54574687c48d07e3cb9e42
sanjanaven <65728470+sanjanaven@users.noreply.github.com>
2021-01-31 10:24:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ $(document).ready(function() {
   // listen for save button clicks
   $(".saveBtn").on("click", function() {
     // get nearby values
-    var textContent = $(this).siblings(".description").val();
-    var name = $(this).parent().attr("id");
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-     
-    localStorage.setItem(name, textContent);
+    // save in localStorage
+    localStorage.setItem(time, value);
   });
 
-
-  
   // load any saved data from localStorage
-  //  $("first.description").val(localStorage.getItem("first"));
-  // JSON.parse(window.localStorage.getItem('first'));
 
-
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  
