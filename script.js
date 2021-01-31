0d54f2b50fa1cd1f01b8d35631a58b6b002beb45
sanjanaven <65728470+sanjanaven@users.noreply.github.com>
2021-01-31 00:24:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,7 @@ $(document).ready(function() {
     localStorage.setItem(time, value);
   });
 
-//$(".dropdown-menu li a").click(function(){
-//  $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-//  $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-//});
+
   
   // load any saved data from localStorage
   // $("#hour-9 .description").val(localStorage.getItem("first"));
